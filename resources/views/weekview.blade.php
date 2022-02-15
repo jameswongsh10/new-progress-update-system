668b97810c67ae2980d0521ed6f396b0cc0ee58c
Lee7000 <700036395@student.curtin.edu.my>
2022-02-15 13:10:30 +0800

    fixed table layout

--- resources/views/weekview.blade.php
+++ resources/views/weekview.blade.php
@@ -5,7 +5,8 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
     <title>Week View</title>
 </head>
@@ -21,7 +22,8 @@
     </a>
     <ul class="collapse nav flex-column ms-1" id="settingmenu" data-bs-parent="#menu">
         <li class="w-100">
-            <a href="{{ route('progress-update-setting.index') }}" class="item-nav custom-font-size px-0">Progress Update Settings</a>
+            <a href="{{ route('progress-update-setting.index') }}" class="item-nav custom-font-size px-0">Progress
+                Update Settings</a>
         </li>
         <li>
             <a href="{{ route('teamsetting.index') }}" class="item-nav custom-font-size px-0">Team Settings</a>
@@ -56,37 +58,41 @@
         </div>
 
         @for($x = 1; $x <= 7; $x++ )
-        <div class="card-body">
-            <h5><?php echo date('Y-m-d', strtotime($_COOKIE['week'] . ' +'.$x.' days'))?></h5>
-            <div class="table-responsive">
-                <table class="table table-striped table-bordered">
-                    <tr>
-                        <th>Tasks</th>
-                        <th>Description</th>
-                        <th>Status</th>
-                        <th>Report</th>
-                    </tr>
-                    @forelse($tasks as $task)
-                        @if(date('Y-m-d', strtotime($_COOKIE['week'] . ' +'.$x.' days')) >= $task->start_date && date('Y-m-d', strtotime($_COOKIE['week'] . ' +'.$x.' days')) <=$task->end_date)
-                            <tr>
-                                <td>{{$task->task_title}}</td>
-                                <td>{{$task->task_description}}</td>
-                                <td>{{$task->status}}</td>
-                                <td><a href="#" class="btn btn-sm btn-success">Download Report</a> </td>
-                            </tr>
-                        @endif
-                    @empty
-                        <p>There is no task</p>
-                    @endforelse
-                </table>
+            <div class="card-body">
+                <h5><?php echo date('Y-m-d', strtotime($_COOKIE['week'] . ' +' . $x . ' days'))?></h5>
+                <div class="table-responsive">
+                    <table class="table table-striped table-bordered">
+                        <tr>
+                            <th>Tasks</th>
+                            <th>Description</th>
+                            <th>Status</th>
+                            <th>Report</th>
+                        </tr>
+                        @forelse($tasks as $task)
+                            @if(date('Y-m-d', strtotime($_COOKIE['week'] . ' +'.$x.' days')) >= $task->start_date && date('Y-m-d', strtotime($_COOKIE['week'] . ' +'.$x.' days')) <=$task->end_date)
+                                <tr>
+                                    <td>{{$task->task_title}}</td>
+                                    <td>{{$task->task_description}}</td>
+                                    <td>{{$task->status}}</td>
+                                    <td><a href="#" class="btn btn-sm btn-success">Download Report</a></td>
+                                </tr>
+                            @endif
+                        @empty
+                            <p>There is no task</p>
+                        @endforelse
+                    </table>
+                </div>
             </div>
-        </div>
         @endfor
 
+    </div>
 </div>
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+        crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"
+        integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 <script type="text/javascript" src="{{asset('js/script.js')}}"></script>
 </body>
 
