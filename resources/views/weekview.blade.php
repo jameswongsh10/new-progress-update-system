333ac3a3afba5a97770822a64f34b88e9c6e38a3
Lee7000 <700036395@student.curtin.edu.my>
2022-02-15 15:43:35 +0800

    merged DashboardController.php

--- resources/views/weekview.blade.php
+++ resources/views/weekview.blade.php
@@ -93,38 +93,6 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 <script type="text/javascript" src="{{asset('js/script.js')}}"></script>
-<script src="http://code.jquery.com/jquery-3.3.1.min.js"
-        integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
-        crossorigin="anonymous">
-</script>
-<script>
-    $.ajaxSetup({
-        headers: {
-            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-        }
-    });
-
-    $(".btn-success").click(function(e){
-
-        e.preventDefault();
-
-        var date = calendar.getDate();
-        var month = date.getMonth();
-        var finalMonth = ++month;
-        var id = {{$user->id}};
-
-        $.ajax({
-            type:'POST',
-            url:"{{route('getData')}}",
-            data:{myMonth:finalMonth, myID:id},
-            success:function(data){
-                console.log("===== " + data + " =====");
-                window.location.href = "{{route('monthlyView')}}";
-            }
-        });
-    });
-    // });
-</script>
 </body>
 
 </html>
